85ac67d7b4325267c3f8fc7dfc3e83850be729f4
Joao <jotavitor2018@gmail.com>
2022-02-18 15:23:52 -0300

    :tada:

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+  
+  mongoose.connect(process.env.MONGO_URL, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+  })
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(morgan("dev"));
+app.use(
+  '/files', express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
+app.use(require("./routes"));
+
+app.listen(3000);
